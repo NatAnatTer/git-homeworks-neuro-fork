11a13abd29e546d59d9e8a174f03a77d986b77c0
Natalia <natinho@yandex.ru>
2022-10-04 23:24:51 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+
 (async () => {
-    // TODO: add custom logic here
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const coordsEl = document.querySelector('#user-coords');
+            coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
+            coordsEl.parentElement.classList.remove('invisible');
+        });
+    }
+(() => {
+    const earlyorderForm = document.querySelector('#earlyorder-form');
+
+    earlyorderForm.addEventListener('submit', ev => {
+        ev.preventDefault();
+
+        const earlyorderContact = earlyorderForm.querySelector('#earlyorder-contact');
+        const contact = earlyorderContact.value.trim();
+
+        if (contact === '') {
+            return;
+        }
+        
+        // TODO: send email simulation:
+        setTimeout(() => {
+            $('#earlyorderModal').modal('show');
+            earlyorderContact.value = '';
+        }, 500);
+    });
 })();
